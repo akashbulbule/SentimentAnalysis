2309352561e373da7bc23d055869367eb0807b25
NAVEENMN <navimn1991@gmail.com>
2014-11-13 13:40:53 -0500

    some-bug

--- Index.java
+++ Index.java
@@ -23,13 +23,22 @@ import java.util.regex.Pattern;
 
 public class Index{
 	
+	public static String bucketid;
+
+    	public void configure(JobConf job) {
+        	//super.configure(job);
+        	bucketid = job.get("bucketid"); 
+   	 }	
+
 	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
 		private Text sentence = new Text();
 		private Text sentiment  = new Text();
+		private String str = null;
 		public void map(LongWritable key, Text value, OutputCollector<Text, Text> output,Reporter reporter)throws IOException{
+			str = get_senti(value.toString());
 			sentence.set(value.toString());
-			sentiment.set("thi");
-			output.collect(sentence, sentiment);
+			sentiment.set(str);
+			if(str == bucketid) output.collect(sentence, sentiment);
 		}	
 	}
 
@@ -40,6 +49,12 @@ public class Index{
 			if(values.hasNext()) output.collect(key, values.next());
 			else throw new IOException("No links found for" + key.toString());
 		}
+	}
+
+	public static String get_senti(String str){
+		String[] columnDetail = new String[11];
+		columnDetail = str.split("\t");
+		return columnDetail[3];
 	}
 	
 
